d4db05dcbc38774a8a57557ac133bf2122c0d66d
Md Tofaal Ahmed <tofaal91522@gmail.com>
2024-07-09 15:33:41 +0600

    yoyo

--- index.js
+++ index.js
@@ -1,36 +1,70 @@
+import cookieParser from "cookie-parser"
 import express from "express"
 import mongoose from "mongoose"
 import path from 'path'
+import jwt from "jsonwebtoken"
 const app = express()
 const port = 3000
-// 
+// sharmin
 app.use(express.static(path.join(path.resolve(), 'public')))
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.set('view engine', 'ejs');
 // mongoose
 mongoose.connect("mongodb://localhost:27017/", {
-    dbName: "bacskend"
-}).then((e) => console.log("Connected to mongoDB")).catch((e) => console.log("Not Connected to mongoDB"))
+    dbName: "Authentication"
+}).then(() => console.log("Connected")).catch(() => console.log("Disconnected"))
 
-const messageSchema = new mongoose.Schema({
+// Registration
+const collectionOfEmaiAndPass = new mongoose.Schema({
+    name: String,
     email: String,
-    password: String
+    password: String,
 })
-const Emails = mongoose.model("Emails", messageSchema)
-// 
-app.set('view engine', 'ejs');
+const saveItems = mongoose.model("Information", collectionOfEmaiAndPass)
 
-app.get('/', (req, res) => {
-    res.render('index.ejs')
-})
-app.post('/contact', (req, res) => {
-    const {email,password} =req.body
-    Emails.create({email,password })
-    res.redirect("success")
+app.post('/registration', async (req, res) => {
+
+    const { name, email, password } = req.body
+    const information = await saveItems.create({ name: name, email: email, password: password })
+
+    const token = jwt.sign({ _id: information._id }, "bGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9eyJfaWQiOiI2Nj")
+
+    res.cookie("token", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 60 * 1000)
+    })
+    res.redirect('/')
 })
 
-app.get('/success', (req, res) => {
-    res.render('success.ejs')
+app.get('/logout', (req, res) => {
+
+    res.cookie("token", null, {
+        httpOnly: true,
+        expires: new Date(Date.now())
+    })
+    res.redirect('/')
 })
+const isAuthenticate = async (req, res, next) => {
+    const { token } = req.cookies
+    if (token) {
+        const decode = jwt.verify(token, "bGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9eyJfaWQiOiI2Nj")
+        req.user = await saveItems.findById(decode._id)
+
+        next()
+    } else {
+        res.render('registration.ejs')
+
+    }
+}
+
+app.get('/', isAuthenticate, (req, res) => {
+    res.render('logout.ejs', { Email: req.user.email })
+})
+// app.get('/login',(req, res) => {
+//     res.render("login.ejs")
+// })
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Server running at ${port}`)
 })
